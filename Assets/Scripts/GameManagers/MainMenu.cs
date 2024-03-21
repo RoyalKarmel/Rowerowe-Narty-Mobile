82143bb47b0bde428fb7b216f60f7bcd43fb2a05
Konrad <kondykox@gmail.com>
2024-03-21 19:11:58 +0100

    logo animations

--- Assets/Scripts/GameManagers/MainMenu.cs
+++ Assets/Scripts/GameManagers/MainMenu.cs
@@ -5,7 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator logo;
     public void PlayGame()
+    {
+        logo.Play("LogoPlay");
+        Invoke("StartGame", 2.5f);
+    }
+
+    void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
