4e4b24a39981d64883192413323693992cb0e6a9
Konrad <kondykox@gmail.com>
2024-04-07 20:30:04 +0200

    optimalize police generator

--- Assets/Scripts/Obstacles/PoliceGenerator.cs
+++ Assets/Scripts/Obstacles/PoliceGenerator.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PoliceGenerator : MonoBehaviour
 {
     public GameObject[] policeGenerators;
-    public List<GameObject> policePrefabs;
+    public GameObject policePrefab;
     public float spawnInterval = 10f;
     private float timer = 0;
     public float heightOffset = 3f;
@@ -25,11 +23,14 @@ public class PoliceGenerator : MonoBehaviour
     {
         int randomIndex = Random.Range(0, policeGenerators.Length);
         GameObject selectedGenerator = policeGenerators[randomIndex];
-        GameObject selectedPolice = policePrefabs[randomIndex];
 
         float minY = selectedGenerator.transform.position.y - heightOffset;
         float maxY = selectedGenerator.transform.position.y + heightOffset;
 
-        Instantiate(selectedPolice, new Vector3(selectedGenerator.transform.position.x, Random.Range(minY, maxY), 0f), selectedGenerator.transform.rotation);
+        GameObject newPolice =
+            Instantiate(policePrefab, new Vector3(selectedGenerator.transform.position.x, Random.Range(minY, maxY), 0f), selectedGenerator.transform.rotation);
+
+        SpriteRenderer policeRenderer = newPolice.GetComponent<SpriteRenderer>();
+        policeRenderer.flipX = randomIndex == 0;
     }
 }
