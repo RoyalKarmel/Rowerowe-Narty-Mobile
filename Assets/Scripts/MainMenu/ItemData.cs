aa19b4ac9603db00cf9dc38d313398c31bc04501
Konrad <kondykox@gmail.com>
2024-04-20 15:09:47 +0200

    deleted unnecessary code

--- Assets/Scripts/MainMenu/ItemData.cs
+++ Assets/Scripts/MainMenu/ItemData.cs
@@ -25,28 +25,4 @@ public class ItemData : MonoBehaviour
         else
             storeManager.SelectItem(itemID);
     }
-
-    #region TODO: Make this works
-    // public void ClickSkin()
-    // {
-    //     StartCoroutine(storeManager.IsItemOwned("skins", itemID, (bool isOwned) =>
-    //     {
-    //         if (!isOwned)
-    //             storeManager.BuyItem(itemID, itemCost, "skins");
-    //         else
-    //             storeManager.SelectItem(itemID, "skins");
-    //     }));
-    // }
-
-    // public void ClickMusic()
-    // {
-    //     StartCoroutine(storeManager.IsItemOwned("musics", itemID, (bool isOwned) =>
-    //     {
-    //         if (!isOwned)
-    //             storeManager.BuyItem(itemID, itemCost, "musics");
-    //         else
-    //             storeManager.SelectItem(itemID, "musics");
-    //     }));
-    // }
-    #endregion
 }
